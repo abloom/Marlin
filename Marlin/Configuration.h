985b4c635344087f955c9564896d7445ae0eb48a
Andrew Bloom <andrew@andrewkbloom.com>
2020-02-04 21:21:08 -0600

    fixing the Y axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1047,7 +1047,7 @@
 
 // Invert the stepper direction. Change (or reverse the motor connector) if an axis goes the wrong way.
 #define INVERT_X_DIR true
-#define INVERT_Y_DIR false
+#define INVERT_Y_DIR true
 #define INVERT_Z_DIR true
 
 // @section extruder
@@ -1074,7 +1074,7 @@
 // Direction of endstops when homing; 1=MAX, -1=MIN
 // :[-1,1]
 #define X_HOME_DIR -1
-#define Y_HOME_DIR -1
+#define Y_HOME_DIR 1
 #define Z_HOME_DIR -1
 
 // @section machine
